892010123e70365c1237a023a901b892a70904e0
Clara Mergner <clara@mergner-of.de>
2019-12-04 18:20:02 +0100

    Aufgabe5g: Merge Konflikt aufgelöst und Ausgabe Moin Moin hinzugefügt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main (String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello World");
-=======
-        System.out.println("Hallo Welt");
->>>>>>> 2399d4ca2f8a485de2efb93f96119f7a0fe68e61
+        System.out.println("Moin Moin");
     }
 }
